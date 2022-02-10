ae2f8ad8cdefb46bfeb79354daa7d4e991653b92
Travis Hall <t.hall89d@gmail.com>
2022-02-10 14:15:51 -0500

    rq

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,32 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const { isLoading, data, isError, error } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes
+  );
+
+  console.log(data, isLoading, isError, error);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}> {hero.name} </div>;
+      })}
+    </>
+  );
+};
